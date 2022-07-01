ab753f1ae2d736d89c1a857b1daf5633a8ae1e6d
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-07-01 12:36:24 +0800

    Create Dataset.sh

---
+++ Dataset.sh
@@ -0,0 +1,15 @@
+mkdir videos
+mkdir video_crops
+mkdir frames
+mkdir choose_frames_all
+mkdir choose_frames
+mkdir annotations
+mkdir choose_frames_middle
+mkdir rawframes
+
+touch ./annotations/train_excluded_timestamps.csv
+touch ./annotations/val_excluded_timestamps.csv
+touch ./annotations/action_list.pbtxt
+touch ./annotations/test_excluded_timestamps.csv
+touch ./annotations/train_excluded_timestamps.csv
+touch ./annotations/val_excluded_timestamps.csv
