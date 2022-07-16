3d23ea2b8aa037fca7d24285e0bd7190f665a4eb
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-07-16 18:19:15 +0800

    Update Dataset.sh

--- Dataset/Dataset.sh
+++ Dataset/Dataset.sh
@@ -8,9 +8,7 @@ mkdir choose_frames_middle
 mkdir rawframes
 mkdir choose_all_middlemost
 
-touch ./annotations/train_excluded_timestamps.csv
-touch ./annotations/val_excluded_timestamps.csv
-touch ./annotations/action_list.pbtxt
-touch ./annotations/test_excluded_timestamps.csv
-touch ./annotations/train_excluded_timestamps.csv
-touch ./annotations/val_excluded_timestamps.csv
+touch ../annotations/train_excluded_timestamps.csv
+touch ../annotations/val_excluded_timestamps.csv
+touch ../annotations/action_list.pbtxt
+touch ../annotations/test_excluded_timestamps.csv
